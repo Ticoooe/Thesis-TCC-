006c63238e342723073f419f8bfb31059721ccde
Paulo Araujo <paulo.araujo@apmhelp.com>
2025-10-02 21:22:10 -0300

    ready to deploy

--- ninho-tech/svelte.config.js
+++ ninho-tech/svelte.config.js
@@ -1,14 +1,22 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
+const config = {
   preprocess: vitePreprocess(),
   
-  // Add compatibility mode for Svelte 4 component API
   compilerOptions: {
     compatibility: {
       componentApi: 4
     }
+  },
+
+  kit: {
+    alias: {
+      "$lib": "src/lib"
+    },
+    files: {
+      assets: "static"
+    }
   }
 }
+
+export default config
